077fd817de308b55da14c3e91a7c3343d7f12930
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-15 14:38:22 -0600

    Intial project setup with new packages

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+class App extends Component {
+
+
+  render() {
+    return(
+      <main>
+        <h1>lol</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
