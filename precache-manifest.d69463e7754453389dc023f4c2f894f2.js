f55c4b8056758d6cfab90cefc4823471f520a2c1
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-20 17:00:00 -0600

    Updates

---
+++ precache-manifest.d69463e7754453389dc023f4c2f894f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec28a2b2fa6679100ef50a995f67155",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "668962d94bafbdf8f14e",
+    "url": "/rick-and-morty/static/css/main.400ece53.chunk.css"
+  },
+  {
+    "revision": "228e52e7aaa5e11046a1",
+    "url": "/rick-and-morty/static/js/2.2a37def6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.2a37def6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668962d94bafbdf8f14e",
+    "url": "/rick-and-morty/static/js/main.b1652ff7.chunk.js"
+  },
+  {
+    "revision": "c1b7ac5fbc2c52e1e347",
+    "url": "/rick-and-morty/static/js/runtime-main.9ea3f8cd.js"
+  }
+]);
