6a504d43b77cd33066d92c78644471ef4b7625c4
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-20 08:16:06 -0600

    Add styling to 404 page and fix broken test

--- src/components/404Page/404Page.js
+++ src/components/404Page/404Page.js
@@ -5,7 +5,7 @@ import './404Page.scss'
 const BadRoute = () => {
   return (
     <article className='bad-route-page'>
-      <h1>Looks Like You Are In The Wrong Neighborhood Yoo</h1>
+      <h1>Looks Like You Are Lost!</h1>
       <Link to={`/`}>
         <button>Go To Home Page</button>
       </Link>
