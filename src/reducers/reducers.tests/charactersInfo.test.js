4cbb689c03fc3919f7a7c80c5167cf1a205c6f3a
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-20 00:48:33 -0600

    Add test for currentCharacter and currentQuery reducers and refactor

--- src/reducers/reducers.tests/charactersInfo.test.js
+++ src/reducers/reducers.tests/charactersInfo.test.js
@@ -1,15 +1,10 @@
 import { charactersInfo } from '../charactersInfo'
 
 describe("charactersInfo", () => {
-  it("should return intial state", () => {
-    const expectedResult = []
-    const result = charactersInfo(undefined, {})
+  let characterInfo1, characterInfo2
 
-    expect(result).toEqual(expectedResult)
-  })
-
-  it("when receiving GET_CHARACTERS_INFO, should return an updated characters array", () => {
-    const characterInfo =
+  beforeEach(() => {
+    characterInfo1 =
     {
       id: 1,
       name: "Rick Sanchez",
@@ -23,29 +18,7 @@ describe("charactersInfo", () => {
       },
     }
 
-    const action = {type:"GET_CHARACTERS_INFO", characterInfo: characterInfo}
-    const expectedResult = [characterInfo]
-    const result = charactersInfo(undefined, action)
-
-    expect(result).toEqual(expectedResult)
-  })
-
-  it("when receiving GET_CHARACTERS_INFO, should return an updated characters array of 2 characters", () => {
-    const characterInfo1 =
-    {
-      id: 1,
-      name: "Rick Sanchez",
-      status: "Alive",
-      species: "Human",
-      type: "",
-      gender: "Male",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1"
-      },
-    }
-
-    const characterInfo2 =
+    characterInfo2 =
     {
       id: 2,
       name: "Morty Smith",
@@ -58,7 +31,24 @@ describe("charactersInfo", () => {
         url: "https://rickandmortyapi.com/api/location/1"
       }
     }
+  })
 
+  it("should return intial state", () => {
+    const expectedResult = []
+    const result = charactersInfo(undefined, {})
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  it("when receiving GET_CHARACTERS_INFO, should return an updated characters array", () => {
+    const action = {type:"GET_CHARACTERS_INFO", characterInfo: characterInfo1}
+    const expectedResult = [characterInfo1]
+    const result = charactersInfo(undefined, action)
+
+    expect(result).toEqual(expectedResult)
+  })
+
+  it("when receiving GET_CHARACTERS_INFO, should return an updated characters array of 2 characters", () => {
     const action = {type:"GET_CHARACTERS_INFO", characterInfo: characterInfo2}
     const expectedResult = [characterInfo1, characterInfo2]
     const result = charactersInfo([characterInfo1], action)
--- src/reducers/reducers.tests/currentCharacter.test.js
+++ src/reducers/reducers.tests/currentCharacter.test.js
@@ -1,12 +1,37 @@
 import { currentCharacter } from '../currentCharacter'
 
 describe("currentCharacter", () => {
+  let characterInfo;
+
+  beforeEach(() => {
+    characterInfo =
+    {
+      id: 1,
+      name: "Rick Sanchez",
+      status: "Alive",
+      species: "Human",
+      type: "",
+      gender: "Male",
+      origin: {
+        name: "Earth (C-137)",
+        url: "https://rickandmortyapi.com/api/location/1"
+      },
+    }
+  })
+
   it("should return intial state", () => {
     const expectedResult = {}
-
     const result = currentCharacter(undefined, {})
 
     expect(result).toEqual(expectedResult)
-
   })
+
+  it("when receiving SET_CURRENT_CHARACTERS_INFO, should return an object of current character", () => {
+    const action = {type:"SET_CURRENT_CHARACTERS_INFO", currentCharacterInfo: characterInfo}
+    const expectedResult = characterInfo
+    const result = currentCharacter(undefined, action)
+
+    expect(result).toEqual(expectedResult)
+  })
+
 })
