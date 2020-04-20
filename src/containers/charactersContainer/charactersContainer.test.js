6dbbaad7c9858e630ca551663813839a9e874b3b
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-20 01:47:12 -0600

    Add test for Query component

--- src/containers/charactersContainer/charactersContainer.test.js
+++ src/containers/charactersContainer/charactersContainer.test.js
@@ -7,7 +7,7 @@ import { createStore } from 'redux';
 import { rootReducer } from '../../reducers/index';
 
 describe('CharactersContainer', () => {
-  it('should display CharactersContainer componets', () => {
+  it('should display CharactersContainer components', () => {
     const testStore = createStore(rootReducer);
     const testWrapper = <Provider store={testStore}><CharactersContainer /></Provider>;
     const { getByText, getByRole } = render(testWrapper);
