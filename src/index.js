c57eb8b5318b07dddf910d3a5893993465796a7c
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-04-20 18:18:52 -0600

    Fix failed test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
-
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const store = createStore(rootReducer, composeWithDevTools());
